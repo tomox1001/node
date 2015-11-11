9ba1874f3602319544113bb3a54eb5f17df65695
kawano_tomonori <kawano_tomonori@cyberagent.co.jp>
2015-11-11 14:09:06 +0900

    Add logger

---
+++ datastore/redis/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Redis = require('ioredis');
+var redisConf = require('../../config').datastore.redis;
+
+var _cache;
+
+exports.getClient = function() {
+  if (!_cache) {
+    console.error('Not connected to redis server');
+    return;
+  }
+
+  return _cache;
+};
+
+/**
+ * connect to the Server
+ */
+exports.connect = function() {
+  _cache = new Redis(redisConf.port, redisConf.hosts);
+};
---
+++ logger/index.js
@@ -0,0 +1,10 @@
+var log4js = require('log4js');
+var config = require('../config');
+
+log4js.configure(config.logger);
+
+module.exports = {
+  access: log4js.getLogger('access'),
+  app: log4js.getLogger('app'),
+  express: log4js.connectLogger(log4js.getLogger('access'), { level: log4js.levels.INFO })
+};
