20143ca4bae8e620a3e7593af1e4e43f90c293f0
warawara <hagiwara_naoki@cyberagent.co.jp>
2015-11-13 13:06:41 +0900

    userの画面

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user', { });
+  res.render('user', {
+    userId: 'user_1',
+    userPublicScore: 52,
+    userImage: 'user_1.png',
+    userFriends: [
+      {
+        userId: 'user_2',
+        userImage: 'user_2.png',
+      },
+    ],
+    recommend: {  // おすすめ
+      itemImages: [
+        'item_1.png',
+        'item_2.png',
+        'item_3.png',
+      ],
+    },
+    post: {  // 最近投稿
+      itemImages: [
+        'item_4.png',
+        'item_5.png',
+        'item_6.png',
+      ],
+    }
+  });
 });
 
 module.exports = router;
