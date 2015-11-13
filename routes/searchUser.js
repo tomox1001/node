b0a07c7c26fd652598b87056cc8fe52e9f0eef4d
warawara <hagiwara_naoki@cyberagent.co.jp>
2015-11-13 14:38:04 +0900

    シナリオ1実装

--- routes/searchUser.js
+++ routes/searchUser.js
@@ -5,15 +5,28 @@ var router = express.Router();
 
 var logger = require('logger');
 
+var userService = require('service/user');
 
 
-
-
+function res_(res, data) {
+  res.json({
+    result: true,
+    data: data,
+  });
+}
 
 
 router.get('/:findByUserId?', function(req, res) {
+  var query = req.query;
+  userService.search(query, function(err, result) {
+    if (err) {
+      logger.app.error(err);
+      res.json({ result: false });
+      return;
+    }
 
-
+    res_(res, result);
+  });
 });
 
 module.exports = router;
