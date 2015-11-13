1a4866bc86fe951515178a9a884be927cb1c2509
warawara <hagiwara_naoki@cyberagent.co.jp>
2015-11-13 14:04:32 +0900

    ガワだけ

---
+++ routes/searchUser.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var logger = require('logger');
+
+
+
+
+
+
+
+router.get('/:findByUserId?', function(req, res) {
+
+
+});
+
+module.exports = router;
