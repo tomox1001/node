6689305cfa5b75b3934fd9d1cc522b2ea92ef5ca
kawano_tomonori <kawano_tomonori@cyberagent.co.jp>
2016-03-17 11:43:39 +0900

    Modified

---
+++ routes/updateItem.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var logger = require('logger');
+var itemService = require('service/item');
+
+router.get('/', (req, res) => {
+  itemService.update(req.query, (err, item) => {
+    if (err) {
+      logger.app.error(err);
+      res.json({ result: false });
+      return;
+    }
+    const result = {
+      result: true,
+      data:[
+        item
+      ]
+    };
+
+    res.json(result);
+  });
+});
+
+module.exports = router;
