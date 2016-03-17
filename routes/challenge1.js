13a921b6c57129a4b289501d756d760de235550f
kawano_tomonori <kawano_tomonori@cyberagent.co.jp>
2016-03-17 15:09:11 +0900

    Modified

--- routes/challenge1.js
+++ routes/challenge1.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 var logger = require('logger');
-var playerService = require('service/item');
+var playerService = require('service/player');
 
 router.get('/', (req, res) => {
   playerService.challengeOne(req.query, (err, item) => {
