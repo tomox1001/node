d71435908a3786b23a013bd8ea9ea48a90ae5568
kawano_tomonori <kawano_tomonori@cyberagent.co.jp>
2016-03-17 15:30:39 +0900

    Modified

---
+++ routes/challenge3.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var logger = require('logger');
+var playerService = require('service/player');
+
+router.get('/', (req, res) => {
+  playerService.challengeThree(req.query, (err, item) => {
+    if (err) {
+      logger.app.error(err);
+      res.json({ result: false });
+      return;
+    }
+    const result = {
+      result: true,
+      data:[
+        item
+      ]
+    };
+
+    res.json(result);
+  });
+});
+
+module.exports = router;
