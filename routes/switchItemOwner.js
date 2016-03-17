c2c473ed61c4c962914be44270e26f5a21381a78
kawano_tomonori <kawano_tomonori@cyberagent.co.jp>
2016-03-17 12:03:21 +0900

    Modified

--- routes/switchItemOwner.js
+++ routes/switchItemOwner.js
@@ -7,7 +7,7 @@ var logger = require('logger');
 var playerService = require('service/player');
 
 router.get('/', (req, res) => {
-  playerService.update(req.query, (err, player) => {
+  playerService.switchItemOwner(req.query, (err, player) => {
     if (err) {
       logger.app.error(err);
       res.json({ result: false });
