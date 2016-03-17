30586baaaef3d6a0d0c6ec78f06aa41f5afdc96d
kawano_tomonori <kawano_tomonori@cyberagent.co.jp>
2016-03-17 14:35:40 +0900

    Modified

---
+++ routes/useItem.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var logger = require('logger');
+var playerService = require('service/player');
+
+router.get('/', (req, res) => {
+  playerService.useItem(req.query, (err, player) => {
+    if (err) {
+      logger.app.error(err);
+      res.json({ result: false });
+      return;
+    }
+    const result = {
+      result: true,
+      data:[
+        player
+      ]
+    };
+
+    res.json(result);
+  });
+});
+
+module.exports = router;
