9604d6e03a75f34963117fee16fdaa1d804a41db
kawano_tomonori <kawano_tomonori@cyberagent.co.jp>
2016-03-17 11:23:35 +0900

    Modified

---
+++ routes/readMap.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var logger = require('logger');
+var mapService = require('service/map');
+
+router.get('/', (req, res) => {
+  mapService.get(req.query, (err, map) => {
+    if (err) {
+      logger.app.error(err);
+      res.json({ result: false });
+      return;
+    }
+    const result = {
+      result: true,
+      data:[
+        map
+      ]
+    };
+
+    res.json(result);
+  });
+});
+
+module.exports = router;
